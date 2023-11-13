b2c0e5eb860c704c55047a3a75455190fb1b59a7
Luna <42366824+KWLuna@users.noreply.github.com>
2023-11-13 17:35:02 +0800

    NI GOU LE HOR. YI ZI ERROR

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-//docker run -d -p 80:80 --name my-apache-php-app -v c:\\Github_KW\\3103-Lab-7b\\src:/var/www/html php:7.2-apache
+# docker run -d -p 80:80 --name my-apache-php-app -v c:\\Github_KW\\3103-Lab-7b\\src:/var/www/html php:7.2-apache
 docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/3103-Lab-7b\\src:/var/www/html php:7.2-apache
 sleep 1
 set +x
